f5437ea6a533b86a4001c8f9c8516e21c3e73e09
vanityDWEED <80824316+vanityDWEED@users.noreply.github.com>
2021-06-20 21:10:51 +0530

    Update Form.js

--- Fruit_Catcher_1-main/js/Form.js
+++ Fruit_Catcher_1-main/js/Form.js
@@ -4,7 +4,7 @@ class Form{
        this.button = createButton('Play');
        this.greeting = createElement('h2');
        this.title = createElement('h2');
-       this.reset = createButton('Reset'); 
+       this.reset = createButton('Reset');
     }
     hide() {
         this.greeting.hide();
@@ -25,10 +25,10 @@ class Form{
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
-        this.reset.position(900,660);
+        this.reset.position(900, 660);
         this.reset.style('width', '100px');
-        this.reset.style('height' , '30px');
-        this.reset.style('background' , 'lightpink');
+        this.reset.style('height', '30px');
+        this.reset.style('background', 'lightpink');
 
         this.button.mousePressed(() => {
             this.input.hide();
@@ -42,6 +42,18 @@ class Form{
             this.greeting.position(400,250);
             this.greeting.style('color', 'white');
             this.greeting.style('font-size', '100px');
+        });
+
+        this.reset.mousePressed(() => {
+            player.updateCount(0);
+            game.update(0);
+
+
+
+            var playerInfoRef = database.ref('players');
+            playerInfoRef.remove();
+
+            
         });
 
     }
